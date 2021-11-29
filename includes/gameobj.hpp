4060f870ec4e81af5efc98497515836aaf14e9b6
Gabriel Dertoni <gab.dertoni@gmail.com>
2021-11-28 23:11:10 -0300

    Add TODO(#3)

--- includes/gameobj.hpp
+++ includes/gameobj.hpp
@@ -23,7 +23,7 @@ public:
 
     // Clone the component. Most implementation should just call the copy
     // constructor of the concrete class.
-    // TODO: Make `virtual std::unique_ptr<Component> clone() const = 0`
+    // TODO(#3): Make `virtual std::unique_ptr<Component> clone() const = 0`
     virtual std::unique_ptr<Component> clone() = 0;
 };
 
