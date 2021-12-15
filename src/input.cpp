271c5b30fb47cc4785fdebe58c1e2cf38731dfa8
GabrielDertoni <gab.dertoni@gmail.com>
2021-12-14 23:31:49 -0300

    Fix segfault on capslock

--- src/input.cpp
+++ src/input.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <bitset>
 
 #include <SFML/Window/Event.hpp>
@@ -10,14 +11,17 @@ namespace input {
     static std::bitset<sf::Keyboard::KeyCount> keysPressed;
 
     void registerKeyPress(sf::Event::KeyEvent key) {
+        if (key.code >= sf::Keyboard::KeyCount || key.code < 0) return;
         keysPressed.set(key.code);
     }
 
     void registerKeyRelease(sf::Event::KeyEvent key) {
+        if (key.code >= sf::Keyboard::KeyCount || key.code < 0) return;
         keysPressed.reset(key.code);
     }
 
     bool isKeyPressed(sf::Keyboard::Key keyCode) {
+        if (keyCode >= sf::Keyboard::KeyCount || keyCode < 0) return false;
         return keysPressed[keyCode];
     }
 }
